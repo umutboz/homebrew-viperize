13254d6221daa2ecf2f07ecdb63f55a6bc0aaa65
umutboz <umut.boz@outlook.com>
2018-08-14 13:41:10 +0300

    1.5.0

--- viperize.rb
+++ viperize.rb
@@ -1,8 +1,8 @@
 class Viperize < Formula
     desc "ios viper code template generator"
     homepage ""
-    url "https://github.com/umutboz/viperize/archive/1.4.0.tar.gz"
-    sha256 "88869bb16aa116c5aa85f71153bb797deb7859e7946043607efae00c1d03d36c"
+    url "https://github.com/umutboz/viperize/archive/1.5.0.tar.gz"
+    sha256 "3ffee9a9c37aef920080266528f56c1ba5bab37d00f588f91a1e1fc8fb86d80a"
     # depends_on "cmake" => :build
     
     def install
